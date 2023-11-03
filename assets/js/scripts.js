dc0d0e7ecc6d5b68f41e0b058bb30c09b6bb079b
Arash <aribesh88@gmail.com>
2023-11-03 23:50:04 +0100

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,294 @@
+"use strict";
+
+// Create the maze matrix
+function createMatrixFromMaze() {
+  const mazeBlocks = document.querySelectorAll(".maze .grid-item");
+  const matrix = [];
+
+  for (let i = 0; i < 10; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < 10; j++) {
+      let index = i * 10 + j;
+      let blockID = mazeBlocks[index].id;
+      let blockClass = mazeBlocks[index].classList[1];
+      matrix[i][j] = blockID + "--" + blockClass;
+    }
+  }
+
+  return matrix;
+}
+
+function movePlayer(player, cellID) {
+  let playerElement = document.getElementById(player);
+  let newCell = document.getElementById(cellID);
+
+  if (newCell) {
+    newCell.appendChild(playerElement);
+  }
+}
+
+function getPlayerPositionID(playerID) {
+  let player = playerID == 0 ? "player-red" : "player-blue";
+  let playerElement = document.getElementById(player);
+  if (playerElement) {
+    let parentID = playerElement.parentElement.id;
+    return parentID;
+  }
+}
+
+/*
+  @param cellID
+  @return sides of the cells with no border (Ex: top, right)
+*/
+function getCellInnerAvailableDirections(cellID) {
+  let currentCellClass = document.getElementById(cellID).classList[1];
+
+  const availableDirections = [];
+
+  // Just check if the main maze border is near!
+
+  if (currentCellClass.length) {
+    let lastFourDigits = currentCellClass.slice(-4);
+
+    if (lastFourDigits[0] === "0") {
+      availableDirections.push("top");
+    }
+
+    if (lastFourDigits[1] === "0") {
+      availableDirections.push("right");
+    }
+
+    if (lastFourDigits[2] === "0") {
+      availableDirections.push("bottom");
+    }
+
+    if (lastFourDigits[3] === "0") {
+      availableDirections.push("left");
+    }
+  }
+
+  return availableDirections;
+}
+
+/*
+  @param matrix, cellID
+  @return adjacent cells' class (Ex: 01-b1010)
+*/
+function getAdjacentCells(matrix, cellID) {
+  let row = Math.floor(cellID / 10);
+  let col = cellID % 10;
+
+  let top = row > 0 ? matrix[row - 1][col] : null;
+  let right = col < matrix[0].length - 1 ? matrix[row][col + 1] : null;
+  let bottom = row < matrix.length - 1 ? matrix[row + 1][col] : null;
+  let left = col > 0 ? matrix[row][col - 1] : null;
+
+  return { top, right, bottom, left };
+}
+
+/*
+  @param adjacentCells, cellID
+  @return ARRAY of the available adjacent cells IDs
+*/
+function getAvailableAdjacentCells(matrix, cellID) {
+  let adjacentCells = getAdjacentCells(matrix, cellID);
+  let availableDirections = getCellInnerAvailableDirections(cellID);
+  let availableCells = [];
+
+  if (
+    adjacentCells.top !== null &&
+    availableDirections.indexOf("top") !== -1
+  ) {
+    let lastFourDigits = adjacentCells.top.slice(-4);
+    if (
+      lastFourDigits.charAt(2) === "0" &&
+      isDeadEnd(adjacentCells.top.slice(0, 2)) == false
+    ) {
+      availableCells.push(adjacentCells.top.slice(0, 2));
+    }
+  }
+
+  if (
+    adjacentCells.right !== null &&
+    availableDirections.indexOf("right") !== -1
+  ) {
+    let lastFourDigits = adjacentCells.right.slice(-4);
+    if (
+      lastFourDigits.charAt(3) === "0" &&
+      isDeadEnd(adjacentCells.right.slice(0, 2)) == false
+    ) {
+      availableCells.push(adjacentCells.right.slice(0, 2));
+    }
+  }
+
+  if (
+    adjacentCells.bottom !== null &&
+    availableDirections.indexOf("bottom") !== -1
+  ) {
+    let lastFourDigits = adjacentCells.bottom.slice(-4);
+    if (
+      lastFourDigits.charAt(0) === "0" &&
+      isDeadEnd(adjacentCells.bottom.slice(0, 2)) == false
+    ) {
+      availableCells.push(adjacentCells.bottom.slice(0, 2));
+    }
+  }
+
+  if (
+    adjacentCells.left !== null &&
+    availableDirections.indexOf("left") !== -1
+  ) {
+    let lastFourDigits = adjacentCells.left.slice(-4);
+    if (
+      lastFourDigits.charAt(1) === "0" &&
+      isDeadEnd(adjacentCells.left.slice(0, 2)) == false
+    ) {
+      availableCells.push(adjacentCells.left.slice(0, 2));
+    }
+  }
+
+  return availableCells;
+}
+
+function isDeadEnd(cellID) {
+  let availableDirections = getCellInnerAvailableDirections(cellID);
+  if (availableDirections.length === 1) {
+    console.log(cellID, "is dead end.");
+    return true;
+  } else return false;
+}
+
+function markAvailableAdjacentCells(cellIDs) {
+  const elementsWithAvailable = document.querySelectorAll(".available");
+  if (elementsWithAvailable.length > 0) {
+    elementsWithAvailable.forEach((el) => {
+      el.classList.remove("available");
+    });
+  }
+  cellIDs.forEach((id) => {
+    let element = document.getElementById(id);
+    if (element) {
+      element.classList.add("available");
+    } else {
+      console.error(`Element with ID ${id} not found.`);
+    }
+  });
+}
+
+// Function to set or update cookies
+const setCookie = (name, value) => {
+  document.cookie = `${name}=${value};`;
+};
+
+// Function to get cookie value
+const getCookie = (name) => {
+  const cookieValue = document.cookie.match(
+    "(^|;)\\s*" + name + "\\s*=\\s*([^;]+)"
+  );
+  return cookieValue ? cookieValue.pop() : "";
+};
+
+function getTurnToPlay() {
+  let turnToPlay = getCookie("turnToPlay");
+  return turnToPlay;
+}
+
+function setTurnToPlay(playerID) {
+  setCookie("turnToPlay", playerID);
+}
+
+function rollDice() {
+  document.getElementById("roll-dice").disabled = true;
+  // Generate a random number between 1 and 6
+  let diceSide = Math.floor(Math.random() * 6) + 1;
+  document.getElementById("dice-side").value = diceSide;
+  return diceSide;
+}
+
+function playPcTurn() {
+  let playerID = getTurnToPlay();
+  let mazeMatrix = createMatrixFromMaze();
+  document.getElementById("player").value = "Player Red [PC]";
+  document.getElementById("roll-dice").disabled = true;
+  let dice = rollDice();
+
+  let count = dice - 1;
+  var traversedCells = [];
+
+  for (let i = 0; i <= count; i++) {
+    function delay() {
+      let playerPositionID = getPlayerPositionID(0);
+      traversedCells.push(playerPositionID);
+      let availableCells = getAvailableAdjacentCells(
+        mazeMatrix,
+        playerPositionID
+      );
+
+      // Remove traversed cells from available cells
+      let newAvailableCells = availableCells.filter(
+        (item) => !traversedCells.includes(item)
+      );
+      console.log(newAvailableCells);
+      let nextStepID = newAvailableCells[0];
+      console.log(nextStepID);
+      movePlayer("player-red", nextStepID);
+    }
+    setTimeout(delay, (i + 1) * 1000);
+  }
+  setTimeout(function () {
+    setTurnToPlay(1);
+    playNextTurn();
+  }, 6000);
+}
+
+function playPlayerOneTurn() {
+  document.getElementById("player").value = "Player Blue [You]";
+  document.getElementById("roll-dice").disabled = false;
+  document.getElementById("dice-side").value = "";
+}
+
+function playNextTurn() {
+  let nextTurn = getTurnToPlay();
+  if (nextTurn == 0) playPcTurn();
+  else playPlayerOneTurn();
+}
+
+function playerMoveOnClick(cellID) {
+  const mazeMatrix = createMatrixFromMaze();
+  const playerPositionID = getPlayerPositionID();
+  let diceSide = document.getElementById("dice-side").value;
+  // Create the maze matrix
+  if (getTurnToPlay() === '1' && diceSide.length >=1) {
+    if (diceSide > 1) {
+      let playerPositionID = getPlayerPositionID(1);
+      let availableCells = getAvailableAdjacentCells(
+        mazeMatrix,
+        playerPositionID
+      );
+      console.log(availableCells);
+      console.log(cellID);
+      if (availableCells.indexOf(cellID) !== -1) {
+        movePlayer("player-blue", cellID);
+        document.getElementById("dice-side").value = diceSide - 1;
+      }
+    } else {
+      setTurnToPlay(0);
+      playNextTurn();
+    }
+  }
+}
+
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("grid-item")) {
+    const cellID = event.target.getAttribute("id");
+    playerMoveOnClick(cellID);
+  }
+});
+
+const gameOptions = {
+  players: 1,
+  difficulty: 0,
+};
+// Reset player turn on reload
+setTurnToPlay(0);
+playNextTurn();
